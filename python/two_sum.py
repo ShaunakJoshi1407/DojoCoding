12d401f06589cc8eb04d45cd499c56ab898a93eb
ShaunakJoshi1407 <shaunakjoshi1407@gmail.com>
2024-01-28 22:43:17 -0600

    Two sum

---
+++ python/two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+
+        for i in range (len(nums)):
+            comp = target - nums[i]
+            if comp in numMap:
+                return [numMap[comp], i]
+            numMap[nums[i]] = i
